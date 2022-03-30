f3b4e734b76e83aa36697cc0fa82f4e6f6e91ab6
julio <chuketoxd15@gmail.com>
2022-03-30 17:02:36 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,136 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [color, setColor] = useState('button')
+
+  const onClickValor = (e) => {
+    let sumatoria = (e.target.innerHTML)
+    let sumaTotal = sumatoria.split(' + ')
+    let suma = 0;
+    for(let i = 0; i < sumaTotal.length; i++){
+      console.log(sumaTotal[i])
+      suma += parseInt(sumaTotal[i]);
+    }
+    
+
+    if(suma === 100){
+      setColor('button-red')
+      }
+  }
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className='container-img' >
+        <img className='img' src='assets/img/precios.png' alt='precios'/>
+      </div>
+      <div>
+        <p className='title'>
+          Si tienes un billete de 💵100 busca 7 opciones de compras que no den
+          vuelto{" "}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='container-button'>
+          <button
+           className={color}
+           onClick={onClickValor}>
+            70
+             + 
+            15
+             + 
+            05
+             + 
+            10
+          </button>
+
+          <button
+           className={color}
+           onClick={onClickValor}>
+            10
+             +
+            70
+             +
+            20
+          </button>
+
+          <button 
+          className={color}
+          onClick={onClickValor}>
+            10
+             +
+            30
+             +
+            05
+          </button>
+
+          <button 
+          className={color}
+          onClick={onClickValor}>
+            95
+             +
+            05
+          </button>
+
+          <button 
+          className={color}
+          onClick={onClickValor}>
+            50
+             +
+            30
+             +
+            20
+          </button>
+
+          <button
+           className={color}
+           onClick={onClickValor}>
+            15
+             +
+            50
+             +
+            05
+             +
+            30
+          </button>
+
+          <button 
+          className={color}
+          onClick={onClickValor}>
+            10
+             +
+            50
+             +
+            15
+             +
+            05
+          </button>
+
+          <button 
+          className={color}
+          onClick={onClickValor}>
+            20
+             +
+            50
+             +
+            15
+             +
+            05
+             +
+            10
+          </button>
+
+          <button 
+          className={color}
+          onClick={onClickValor}>
+            70
+             +
+            30
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
